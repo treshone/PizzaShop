5caa977a1cdfed8154acb0e01cbf5aae2b00b15a
Alexandr <treshone994s@gmail.com>
2018-03-27 17:50:49 +0500

    add add_to_cart function stub

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
 function something()
 {
-	var x = window.localStorage.getItem('v');
+	var x = window.localStorage.getItem('b');
 
 	x = x * 1 + 1;
 
-	window.localStorage.setItem('v', x);
+	window.localStorage.setItem('b', x);
 
 	alert(x);
+}
+
+function add_to_cart()
+{
+	alert('Hello from function!');
 }
